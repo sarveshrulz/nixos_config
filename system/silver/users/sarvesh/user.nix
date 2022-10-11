b3cbea2752df96972319109c1728d9623da9dd98
sarveshrulz <sarveshkardekar@gmail.com>
2022-10-11 23:03:57 +0530

    Silver: Sarvesh: remove text files in config

--- system/silver/users/sarvesh/user.nix
+++ system/silver/users/sarvesh/user.nix
@@ -234,37 +234,168 @@
     xdg.configFile = {
       "hypr/scripts/brightness.sh" = {
         executable = true;
-        source = ./files/config/hypr/scripts/brightness.sh;
+        text = ''
+          #!/usr/bin/env bash
+          msgId="69"
+          xbacklight -fps 60 "$@"
+          curr="$(xbacklight -get)"
+          dunstify -a "changeBrightness" -i ~/.config/dunst/icons/brightness.png -u low -r "$msgId" "Brightness: $curr%"
+        '';
       };
       "hypr/scripts/volume.sh" = {
         executable = true;
-        source = ./files/config/hypr/scripts/volume.sh;
+        text = ''
+          #!/usr/bin/env bash
+          msgId="69"
+          pamixer $@ > /dev/null
+          if [ $(pamixer --get-mute) = true ] && [ ! $@ = '-t' ]; then
+               pamixer -t
+          fi
+          volume=$(pamixer --get-volume)
+          mute=$(pamixer --get-mute)
+          if [ $volume = 0 ] || [ $mute = true ]; then
+              dunstify -a "changeVolume" -i ~/.config/dunst/icons/muted.png -u low -r "$msgId" "Volume: muted" 
+          else
+              dunstify -a "changeVolume" -i ~/.config/dunst/icons/volume.png -u low -r "$msgId" "Volume: $volume%"
+          fi
+        '';
       };
       "hypr/autostart" = {
         executable = true;
-        source = ./files/config/hypr/autostart;
+        text = ''
+          #!/usr/bin/env bash
+          dbus-update-activation-environment --systemd DISPLAY WAYLAND_DISPLAY HYPRLAND_INSTANCE_SIGNATURE XDG_CURRENT_DESKTOP &
+          dunst &
+          wl-paste -t text --watch clipman store &
+          gammastep -l 19:72 &
+          ~/.nix-profile/libexec/polkit-gnome-authentication-agent-1 &
+          foot -s &
+          thunar --daemon &
+          waybar &
+          swaybg -o \* -i ~/Pictures/Wallpapers/default -m fill
+        '';
       };
-      "rofi/scripts/rofi-power-menu" = {
-        executable = true;
-        source = ./files/config/rofi/scripts/rofi-power-menu;
-      };
-      "rofi/scripts/rofi-bluetooth" = {
-        executable = true;
-        source = ./files/config/rofi/scripts/rofi-bluetooth;
-      };
-      "rofi/scripts/rofi-wifi-menu" = {
-        executable = true;
-        source = ./files/config/rofi/scripts/rofi-wifi-menu;
-      };
-      "hypr/hyprland.conf".source = ./files/config/hypr/hyprland.conf;
-      "rofi/apps.rasi".source = ./files/config/rofi/apps.rasi;
-      "rofi/powermenu.rasi".source = ./files/config/rofi/powermenu.rasi;
-      "rofi/bluetooth.rasi".source = ./files/config/rofi/bluetooth.rasi;
-      "rofi/wlan.rasi".source = ./files/config/rofi/wlan.rasi;
-      "dunst/icons/brightness.png".source = ./files/config/dunst/icons/brightness.png;
-      "dunst/icons/muted.png".source = ./files/config/dunst/icons/muted.png;
-      "dunst/icons/volume.png".source = ./files/config/dunst/icons/volume.png;
-      "weston/background.jpg".source = ./files/config/weston/background.jpg;
+      "hypr/hyprland.conf".text = ''
+        monitor=,1920x1080@60,0x0,1
+        input {
+            follow_mouse=1
+            touchpad {
+              natural_scroll=1
+            }
+        }
+        gestures {
+            workspace_swipe=1
+        }
+        misc {
+            disable_hyprland_logo=1
+            disable_splash_rendering=1
+            disable_autoreload=1
+        }
+        general {
+            sensitivity=1.0
+            gaps_in=6
+            gaps_out=12
+            border_size=0
+            damage_tracking=full
+        }
+        decoration {
+            rounding=12
+            drop_shadow=0
+            blur=0
+            multisample_edges=0
+        }
+        animations {
+            enabled=1
+            animation=windows,1,7,default
+            animation=fade,1,10,default
+            animation=workspaces,1,6,default
+        }
+        workspace=,1
+        windowrule=float,^(Rofi)$
+        windowrule=float,^(Dunst)$
+        bind=SUPER,RETURN,exec,footclient
+        bind=SUPER,SPACE,exec,rofi -no-lazy-grab -show drun -modi drun -theme ~/.config/rofi/apps.rasi
+        bind=SUPER,Q,killactive,
+        bind=SUPER,left,movefocus,l
+        bind=SUPER,right,movefocus,r
+        bind=SUPER,up,movefocus,u
+        bind=SUPER,down,movefocus,d
+        bind=SUPER_SHIFT,left,movewindow,l
+        bind=SUPER_SHIFT,right,movewindow,r
+        bind=SUPER_SHIFT,up,movewindow,u
+        bind=SUPER_SHIFT,down,movewindow,d
+        bind=SUPER,1,workspace,1
+        bind=SUPER,2,workspace,2
+        bind=SUPER,3,workspace,3
+        bind=SUPER,4,workspace,4
+        bind=SUPER,5,workspace,5
+        bind=SUPER,6,workspace,6
+        bind=SUPER,7,workspace,7
+        bind=SUPER,8,workspace,8
+        bind=SUPER,9,workspace,9
+        bind=SUPER,0,workspace,10
+        bind=SUPER_SHIFT,1,movetoworkspacesilent,1
+        bind=SUPER_SHIFT,2,movetoworkspacesilent,2
+        bind=SUPER_SHIFT,3,movetoworkspacesilent,3
+        bind=SUPER_SHIFT,4,movetoworkspacesilent,4
+        bind=SUPER_SHIFT,5,movetoworkspacesilent,5
+        bind=SUPER_SHIFT,6,movetoworkspacesilent,6
+        bind=SUPER_SHIFT,7,movetoworkspacesilent,7
+        bind=SUPER_SHIFT,8,movetoworkspacesilent,8
+        bind=SUPER_SHIFT,9,movetoworkspacesilent,9
+        bind=SUPER_SHIFT,0,movetoworkspacesilent,10
+        bind=SUPER_SHIFT,Q,exit,
+        bind=SUPER,F,togglefloating,
+        bind=SUPER,M,fullscreen,
+        bind=SUPER,PRINT,exec,grim ~/Pictures/Screenshots/$(date +'%s_grim.png') && dunstify "Screenshot saved!"
+        bind=,XF86MonBrightnessUp,exec,~/.config/hypr/scripts/brightness.sh -inc 2
+        bind=,XF86MonBrightnessDown,exec,~/.config/hypr/scripts/brightness.sh -dec 2
+        bind=,XF86AudioMute,exec,~/.config/hypr/scripts/volume.sh -t
+        bind=,XF86AudioRaiseVolume,exec,~/.config/hypr/scripts/volume.sh -i 5
+        bind=,XF86AudioLowerVolume,exec,~/.config/hypr/scripts/volume.sh -d 5
+        exec-once=/home/sarvesh/.config/hypr/autostart
+      '';
+      "rofi/apps.rasi".text = ''
+        configuration {
+        	font: "Noto Sans Semi-bold 11";
+        	me-select-entry: "MouseSecondary";
+        	me-accept-entry: "MousePrimary";
+          drun-display-format: "{name}";
+        }
+        window {
+          width: 224px;
+        }
+        * {
+          background: #0a0a0a;
+          foreground: #b0b0b0;
+        }
+        entry {
+          padding: 12px 12px 0px 12px;
+        }
+        prompt, textbox-prompt-colon, case-indicator {
+          enabled: false;
+        }
+        mainbox, entry, listview, element {
+          background-color: @background;
+          text-color: @foreground;
+        }
+        listview {
+          lines: 4;
+          padding: 12px;
+          spacing: 4px;
+          cycle: false;
+        }
+        element-text selected {
+          background-color: @foreground;
+          text-color: @background;
+          border-radius: 12px;
+        }
+        element-text {
+          padding: 6px 12px;
+          background-color: @background;
+          text-color: @foreground;
+        }
+      '';
       "weston.ini".text = ''
         [libinput]
         enable-tap=true
@@ -281,6 +412,10 @@
           WAYLAND_DISPLAY="wayland-1" waydroid show-full-ui
         '';
       };
+      "weston/background.jpg".source = ./files/config/weston/background.jpg;
+      "dunst/icons/brightness.png".source = ./files/config/dunst/icons/brightness.png;
+      "dunst/icons/muted.png".source = ./files/config/dunst/icons/muted.png;
+      "dunst/icons/volume.png".source = ./files/config/dunst/icons/volume.png;
     };
 
     gtk =
