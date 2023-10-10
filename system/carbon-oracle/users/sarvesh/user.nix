92940249baf80dcbe4ea36c2e8ec987edeee4dd3
sarveshrulz <sarveshkardekar@gmail.com>
2023-10-10 19:25:42 +0530

    Carbon-oracle: nextcloud-aio

--- system/carbon-oracle/users/sarvesh/user.nix
+++ system/carbon-oracle/users/sarvesh/user.nix
@@ -6,6 +6,26 @@
     ".ssh/id_rsa.pub".text = secrets.carbon.sarvesh.sshKeys.public;
   };
 
+  networking.firewall = {
+    allowedTCPPorts = [ 80 443 ];
+    allowedUDPPorts = [ 443 ];
+  };
+
+  virtualisation = {
+    docker.enable = true;
+    oci-containers = {
+      backend = "docker";
+      containers = {
+        nextcloud-aio-mastercontainer = {
+          autoStart = true;
+	  image = "nextcloud/all-in-one:latest";
+	  ports = [ "80:80" "8080:8080" "8443:8443" ];
+	  volumes = [ "nextcloud_aio_mastercontainer:/mnt/docker-aio-config" "//var/run/docker.sock:/var/run/docker.sock:ro" ];
+	};
+      };
+    };
+  };
+
   users.users.sarvesh = {
     hashedPassword = secrets.carbon.sarvesh.password;
     openssh.authorizedKeys.keys = [ secrets.carbon.sarvesh.sshKeys.public ];
